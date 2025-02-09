f6cbc5b49d2c91fa594658fd20e6b8d643244639
Petros_Papa <petrospapa21@gmail.com>
2025-02-09 14:32:03 +0200

    actions

--- Assets/Scripts/UserActions.cs
+++ Assets/Scripts/UserActions.cs
@@ -114,6 +114,16 @@ public class UserActions: MonoBehaviour
         });
     }
 
+    public void UserClickedAction(String action)
+    {
+
+    }
+
+    public void UserClickedLock()
+    {
+
+    }
+
     private void ObserveRounds(string roomId)
     {
         Debug.Log($"observer 1" + roomId);
